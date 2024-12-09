7f5f7745bd38987abee2a9ad064514a1705809ff
YvonneJL <yvonneloder@gmx.de>
2024-12-09 14:55:09 +0100

    fertig

---
+++ Aufgabe_9/assets/javascript/main.js
@@ -0,0 +1,23 @@
+// window.alert("Hallo Welt");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// !Bei dieser Variante erschient Platzhalter --> zweites Wort, also cancel
+
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+// let alter = window.prompt("gib mir bitte dein Alter:"); 
+// console.log(alter);
+// !zeigt dann das Alter ein, dass angegeben wird als Var alter
+
+let b = 5;
+let a = b * 5 - 3;
+
+window.alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt :";
+// let grosse = "2,72m";
+// window.confirm(mann + grosse);
+
+// !Hier kann man zuvor bestimmte Variablen anzeigen
